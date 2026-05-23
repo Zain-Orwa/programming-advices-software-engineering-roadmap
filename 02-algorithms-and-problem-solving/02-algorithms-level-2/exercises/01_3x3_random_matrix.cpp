2bf8a54925510a32680b18a3f65249917e510345
Zain Orwa <orwazain3@gmail.com>
2026-05-23 18:21:54 +0200

    Add algorithm roadmap level folders

---
+++ 02-algorithms-and-problem-solving/02-algorithms-level-2/exercises/01_3x3_random_matrix.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <ctime>
+
+using namespace std;
+
+int	RandomNumber(int From, int To)
+{
+	return (rand() % (To - From + 1) + From);
+}
+
+void	FillMatirxWithRandomNumbers(int arr[3][3], short rows, short cols)	
+{
+	while (
+}
+
+int main(void)
+{
+	srand(time(0));
+
+	cout << RandomNumber(1, 7) << endl;
+	cout << RandomNumber(1, 7) << endl;
+	cout << RandomNumber(1, 7) << endl;
+
+	return (0);
+}
---
+++ 02-algorithms-and-problem-solving/03-algorithms-level-3/exercises/01_3x3_random_matrix.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+
+int	RandomNUmber(int From, int To)
+{
+	int	randomNum;
+
+	randomNum = rand() % (To - From) + From;
+}
