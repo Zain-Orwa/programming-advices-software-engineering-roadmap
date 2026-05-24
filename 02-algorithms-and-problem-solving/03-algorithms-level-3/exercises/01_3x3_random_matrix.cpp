a80a6fb9f7adf7915986ca79d2aab1fa78f72462
Zain Orwa <orwazain3@gmail.com>
2026-05-24 17:36:43 +0200

    Random Matrix

--- 02-algorithms-and-problem-solving/03-algorithms-level-3/exercises/01_3x3_random_matrix.cpp
+++ 02-algorithms-and-problem-solving/03-algorithms-level-3/exercises/01_3x3_random_matrix.cpp
@@ -3,9 +3,37 @@
 #include <string>
 
 
-int	RandomNUmber(int From, int To)
+int	RandomNumber(int From, int To)
 {
-	int	randomNum;
+	return (rand() % (To - From) + From);
+}
 
-	randomNum = rand() % (To - From) + From;
+void	FillMatrixWithRandomNumbers(int arr[3][3], short Rows, short Cols)
+{
+	for (short i = 0; i < Rows; i++)
+		for (short j = 0; j < Cols; j++)
+			arr[i][j] = RandomNumber(1, 100);
+}
+
+void	PrintMatrix(int arr[3][3], short Rows, short Cols)
+{
+	for (short i = 0; i < Rows; i++)
+	{	
+		for (short j = 0; j < Cols; j++)
+			std::cout << std::setw(3) << arr[i][j] << "		";
+		std::cout << "\n";
+	}
+}
+
+int main(void)
+{
+	//seed the random number generator, only once
+	srand((unsigned)time(NULL));
+	
+	int arr[3][3];
+	FillMatrixWithRandomNumbers(arr, 3, 3);
+	std::cout << "\n The following is a 3x3 random matrix:\n";
+	PrintMatrix(arr, 3, 3);
+
+	return (0);
 }
