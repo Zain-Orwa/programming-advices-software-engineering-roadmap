920d0fa337de9edfe099d87178b467b9fb326352
Zain Orwa <zorwa@c3r10p7.42vienna.com>
2026-06-14 12:41:57 +0200

    adding sum each cols in the matrix in arr

--- 02-algorithms-and-problem-solving/03-algorithms-level-3/exercises/05_sum_matrix_cols_in_array.cpp
+++ 02-algorithms-and-problem-solving/03-algorithms-level-3/exercises/05_sum_matrix_cols_in_array.cpp
@@ -27,7 +27,7 @@ void	PrintMatrix(int arr[3][3], short Rows, short Cols)
 	
 }
 
-int	ColSum(int arr[3][3], short Rows, short ColNumber)
+int	ColSum(int arr[3][3], short Rows,  short ColNumber)
 {
 	int	Sum = 0;
 	for (short i = 0; i < Rows; i++)
@@ -37,13 +37,17 @@ int	ColSum(int arr[3][3], short Rows, short ColNumber)
 	return (Sum);
 }
 
-void	PrintEachColSum(int arr[3][3], short Rows, short Cols)
+void	SumMatrixColsInArray(int arr[3][3],int arrSum[3], short Rows, short Cols)
 {
-	std::cout << "\nThe following is sum of each Col in the matrix:\n";
-	for (short j = 0; j < Cols; j++)
-	{
-		std::cout << " Col " << j + 1 << " Sum "<< ColSum(arr, Rows, j) << std::endl;
-	}
+	for (short j = 0; j < Cols; j ++)
+		arrSum[j] = ColSum(arr, Rows, j);
+}
+
+void	PrintColsSumArray(int arr[3], short Rows)
+{
+	std::cout << "\nThe following is sum of each row in the matrix:\n";
+	for (short i = 0; i < Rows; i++)
+		std::cout << " Row " << i + 1 << " Sum = " << arr[i] << std::endl;
 }
 
 
@@ -52,12 +56,14 @@ int main(void)
 	srand((unsigned)time(NULL));
 
 	int arr[3][3];
+	int arrSum[3];
 
 	FillMatrixWithRandomNumbers(arr, 3, 3);
 	std::cout << "\nThe following is 3x3 random matrix:\n";
 	PrintMatrix(arr, 3, 3);
 
-	PrintEachColSum(arr, 3, 3);
+	SumMatrixColsInArray(arr, arrSum, 3, 3);
+	PrintColsSumArray(arrSum, 3);
 
 	return (0);
 }
