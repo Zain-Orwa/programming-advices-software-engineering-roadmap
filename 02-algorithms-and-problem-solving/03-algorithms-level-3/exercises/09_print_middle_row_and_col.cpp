29b6ba2ab4d321c902173b60fa57da0678519b75
Orwa Zain <orwazain3@gmail.com>
2026-06-15 03:38:56 +0200

    print middle row and col

---
+++ 02-algorithms-and-problem-solving/03-algorithms-level-3/exercises/09_print_middle_row_and_col.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <iomanip>
+
+
+int	RandomNumber(int From, int To)
+{
+	return (rand() % (To - From) + From);
+}
+
+void	FillMatrixWithRandomNumbers(int arr[3][3], short Rows, short Cols)
+{
+	for (short i = 0; i < Rows; i++)
+		for (short j = 0; j < Cols; j++)
+			arr[i][j] = RandomNumber(1, 10);
+}
+
+void	PrintMatrix(int arr[3][3], short Rows, short Cols)
+{
+	for (short i = 0; i < Rows; i++)
+	{	
+		for (short j = 0; j < Cols; j++)
+		{
+			printf(" %0*d ", 2, arr[i][j]);
+			//std::cout << std::setw(3) << arr[i][j] << "		";
+		}
+		std::cout << "\n";
+	}
+}
+
+void	PrintMiddleRow(int arr[3][3], short Rows)
+{
+	for (short i = 0; i < Rows; i++)
+		printf(" %0*d ", 2, arr[1][i]);
+	printf("\n");
+	
+}
+
+void	PrintMiddleCol(int arr[3][3], short Cols)
+{
+	for (short i = 0; i < Cols; i++)
+		printf(" %0*d \n", 2, arr[i][1]);
+}
+
+int main(void)
+{
+	//seed the random number generator, only once
+	srand((unsigned)time(NULL));
+	
+	int arr[3][3];
+
+	FillMatrixWithRandomNumbers(arr, 3, 3);
+	std::cout << "\nMatrix1:\n";
+	PrintMatrix(arr, 3, 3);
+
+	std::cout << "\nMiddle Row:\n";
+	PrintMiddleRow(arr, 3);
+
+	std::cout << "\nMiddle Col:\n";
+	PrintMiddleCol(arr, 3);
+
+	return (0);
+}
