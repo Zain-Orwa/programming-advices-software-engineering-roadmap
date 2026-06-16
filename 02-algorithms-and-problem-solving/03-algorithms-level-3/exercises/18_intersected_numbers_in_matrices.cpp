433de3a4b2b5d97c389e01c542aa6bc3bd90c432
Orwa Zain <orwazain3@gmail.com>
2026-06-16 16:02:15 +0200

    print intersected numbers in matrix

--- 02-algorithms-and-problem-solving/03-algorithms-level-3/exercises/18_intersected_numbers_in_matrices.cpp
+++ 02-algorithms-and-problem-solving/03-algorithms-level-3/exercises/18_intersected_numbers_in_matrices.cpp
@@ -13,10 +13,37 @@ void PrintMatrix(int arr[3][3], short Rows, short Cols)
     }
 }
 
+bool IsNumberInMatrix(int arr[3][3], short num, short Rows, short Cols)
+{
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            if (arr[i][j] == num)
+                return (true);
+        }
+    }
+    return (false);
+}
+void    PrintIntersectedNumbers(int arr1[3][3], int arr2[3][3], short Rows, short Cols)
+{
+    short Number;
+
+    for (short i = 0; i < Rows; i++)
+    { 
+        for (short j = 0; j < Cols; j++)
+        {
+            Number = arr1[i][j];
+            if (IsNumberInMatrix(arr2, Number, Rows, Cols))
+                std::cout << Number << "    ";
+        }
+    }
+}
+
 int main(void)
 {
-    int arr1[3][3] = {{9, 0, 0}, {0, 4, 0}, {0, 1, 9}};
-    int arr2[3][3] = {{9, 5, 0}, {0, 4, 0}, {0, 8, 9}};
+    int arr1[3][3] = { {77,5,12},{22,20,1},{1,0,9} };
+    int arr2[3][3] = { {5,80,90},{22,77,1},{10,8,33} };
 
 
 	std::cout << "\nMatrix1:\n";
@@ -25,6 +52,8 @@ int main(void)
 	std::cout << "\nMatrix2:\n";
 	PrintMatrix(arr2, 3, 3);
 
+    std::cout << "\nIntersected numbers are:\n";
+    PrintIntersectedNumbers(arr1, arr2, 3, 3);
 
     return (0);
 }
