d99f636a62b2b6843203a7646ddc62a907e43f1d
Orwa Zain <orwazain3@gmail.com>
2026-06-16 11:40:08 +0200

    check identity matrix

--- 02-algorithms-and-problem-solving/03-algorithms-level-3/exercises/13_check_identity_matrix.cpp
+++ 02-algorithms-and-problem-solving/03-algorithms-level-3/exercises/13_check_identity_matrix.cpp
@@ -22,13 +22,34 @@ void PrintMatrix(int arr[3][3], short Rows, short Cols)
     }
 }
 
+bool    IsIdentityMatrix(int arr[3][3], short Rows, short Cols)
+{
+        for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            if (i == j && arr[i][j] != 1)
+                return (false);
+            else if (i != j && arr[i][j] != 0)
+                return (false);
+        }
+    }
+    return (true);
+}
+
 int main(void)
 {
-    int arr[3][3];
+    //int arr[3][3];
+    int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
 
-    CreateIdentityMatrix(arr, 3, 3);
+   // CreateIdentityMatrix(arr, 3, 3);
     std::cout << "\nThe following is 3x3 matrix:\n";
     PrintMatrix(arr, 3, 3);
+
+    if (IsIdentityMatrix(arr, 3, 3))
+        std::cout << "\nYes, matrix is identity!\n";
+    else
+        std::cout << "\nNo, matrix is not identity!\n";
 
     return (0);
 }
