9af17c5a2c4ccd6b12a412373c43d8f38371ebba
Orwa Zain <orwazain3@gmail.com>
2026-07-09 05:26:56 +0200

    bank project v:1

--- 02-algorithms-and-problem-solving/projects/bank_01/console_application_bank1.cpp
+++ 02-algorithms-and-problem-solving/projects/bank_01/console_application_bank1.cpp
@@ -430,6 +430,73 @@ void ShowDeleteClientScreen()
     DeleteClientByAccountNumber(AccountNumber, vClients);
 }
 
+sClient ChangeClientRecord(string AccountNumber)
+{
+    sClient Client;
+
+    Client.AccountNumber = AccountNumber;
+
+    cout << "\n\nEnter PinCode? ";
+    getline(cin >> ws, Client.PinCode);
+
+    cout << "Enter Name? ";
+    getline(cin, Client.Name);
+
+    cout << "Enter Phone? ";
+    getline(cin, Client.Phone);
+
+    cout << "Enter AccountBalance? ";
+    cin >> Client.AccountBalance;
+
+    return Client;
+}
+
+bool UpdateClientByAccountNumber(string AccountNumber, vector <sClient>& vClients)
+{
+    sClient Client;
+    char Answer = 'n';
+
+    if (FindClientByAccountNumber(AccountNumber, vClients, Client))
+    {
+        PrintClientCard(Client);
+
+        cout << "\n\nAre you sure you want update this client? y/n ? ";
+        cin >> Answer;
+
+        if (Answer == 'y' || Answer == 'Y')
+        {
+            for (sClient& C : vClients)
+            {
+                if (C.AccountNumber == AccountNumber)
+                {
+                    C = ChangeClientRecord(AccountNumber);
+                    break;
+                }
+            }
+
+            SaveCleintsDataToFile(ClientsFileName, vClients);
+
+            cout << "\n\nClient Updated Successfully.";
+            return true;
+        }
+    }
+    else
+        cout << "\nClient with Account Number (" << AccountNumber << ") is Not Found!";
+    return false;
+}
+
+void ShowUpdateClientScreen()
+{
+    cout << "\n-----------------------------------\n";
+    cout << "\tUpdate Client Info Screen";
+    cout << "\n-----------------------------------\n";
+
+    vector <sClient> vClients = LoadCleintsDataFromFile(ClientsFileName);
+    string AccountNumber = ReadClientAccountNumber();
+
+    UpdateClientByAccountNumber(AccountNumber, vClients);
+}
+
 void    PerformMainMenueOption(enMainMenueOptions MainMenueOption)
 {
     switch (MainMenueOption)
@@ -454,6 +521,7 @@ void    PerformMainMenueOption(enMainMenueOptions MainMenueOption)
 
         case enMainMenueOptions::eUpdateClient:
         ClearScreen();
+        ShowUpdateClientScreen();
         GoBackToMainMenue();
         break;
 
