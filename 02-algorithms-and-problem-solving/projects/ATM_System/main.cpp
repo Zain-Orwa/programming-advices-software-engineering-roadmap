7e79d75cf86db1e990192f6112089f2e645fc748
Orwa Zain <orwazain3@gmail.com>
2026-07-18 17:14:36 +0200

    ATM System

--- 02-algorithms-and-problem-solving/projects/ATM_System/main.cpp
+++ 02-algorithms-and-problem-solving/projects/ATM_System/main.cpp
@@ -1,7 +1,24 @@
 #include "atm.h"
 
+void    login()
+{
+    int     AccountNumber;
+    bool    LoginFaild;
+    
+    AccountNumber = 0;
+    LoginFaild = false;
+    do
+    {
+
+
+
+    } while (LoginFaild);
+
+
+}
 
 int main()
 {
+
     return 0;
 }
