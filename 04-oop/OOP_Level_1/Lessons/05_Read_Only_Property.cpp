be78a08a4e047547088a4ec4e919846733326f14
Orwa Zain <orwazain3@gmail.com>
2026-07-29 10:47:55 +0200

    Read Only Properties

---
+++ 04-oop/OOP_Level_1/Lessons/05_Read_Only_Property.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+class clsPerson
+{
+private:
+   string   _FirstName;
+   string   _LastName;
+   int      _ID = 10;
+   
+public:
+    // Set first name //
+    void    SetFirstName(string FirstName)
+    {
+        _FirstName = FirstName;
+    }
+
+    string    GetFirstName()
+    {
+        return (_FirstName);
+    }
+
+    // Set last name //
+    void    SetLastName(string LastName)
+    {
+        _LastName = LastName;
+    }
+
+    string    GetLastName()
+    {
+        return (_LastName);
+    }
+
+    string  FullName()
+    {
+        return (_FirstName + " " + _LastName);
+    }
+
+    int GetID() // we have only get function (_ID) there is no set function and this read-only id//
+    {
+        return (_ID);
+    }
+};
+
+
+int main(void)
+{
+    clsPerson Person1;
+
+    Person1.SetFirstName("John");
+    Person1.SetLastName("Deo");
+
+    cout << "First Name: " << Person1.GetFirstName() << endl;
+    cout << "Last Name : " << Person1.GetLastName() << endl;
+    cout << "Full Name : " << Person1.FullName() << endl;
+    cout << "ID        : " << Person1.GetID() << endl;
+
+    return (0);
+}
