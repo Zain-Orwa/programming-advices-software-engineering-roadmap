f618b94af6aecc5eaf14c524c999c5354d27cd36
Orwa Zain <orwazain3@gmail.com>
2026-07-29 12:58:47 +0200

    Properties: [set] and [get] using [=]

--- 04-oop/OOP_Level_1/Lessons/05_Read_Only_Property.md
+++ 04-oop/OOP_Level_1/Lessons/05_Read_Only_Property.md
@@ -1,4 +1,2 @@
 # Read Only Properties:
-    as we set the variable _ID as a private attributes(variable), we can initialize the id from the beginning or we can get from our file, and we create only getter property function without the set one so this gives us more scure and control over the code:
-
-    
+    as we set the variable _ID as a private attributes(variable), we can initialize the id from the beginning or we can get from our file, and we create only getter property function without the set one so this gives us more scure and control over the code.
---
+++ 04-oop/OOP_Level_1/Lessons/06_Properties_Set_and_Get_through_=_.cpp
@@ -0,0 +1,49 @@
+// Here we are going to use declaration specification properties that will helps instead of calling
+// the function itself like (SetFirstName) and (GetFirstName), we just use when keyword name and depends
+// on the parameters in the case of the set(assign) with the "=" equal sign is going to set the name.
+// and is the case of get we just we use that key word again without assignning:
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class clsPerson
+{
+private:
+    string _FirstName;
+
+public:
+   void   SetFirstName(string FirstName) 
+   {
+        _FirstName = FirstName;
+   }
+
+   string   GetFirstName()
+   {
+        return (_FirstName);
+   }
+
+   // this only works with MSVC, but we can use clang++ like this with this flag[-fdeclspec]:
+   // clang++ -fdeclspec 06_Properties_Set_and_Get_through_=_.cpp
+   // bare in mind at this point we can't use flags like: -Wall -Wextra -Werror
+
+   __declspec(property(get = GetFirstName, put = SetFirstName)) string FirstName;
+};
+
+
+int main(void)
+{
+    clsPerson Person1;
+
+    cout << "1. Without using __declspec: ";
+    Person1.SetFirstName("Alex");
+    cout << Person1.GetFirstName() << endl;
+
+    cout << "\n2. Using __declspec: ";
+    Person1.FirstName = "Alex";
+    cout << Person1.FirstName << endl;
+
+    return (0);
+}
