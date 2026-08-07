a1ba438fc7f4829c3dc92e731f5548ce2d707f97
Orwa Zain <orwazain3@gmail.com>
2026-08-07 16:51:42 +0200

    Nested Classes

--- 04-oop/OOP_Level_1/Lessons/29_Nested_Class.cpp
+++ 04-oop/OOP_Level_1/Lessons/29_Nested_Class.cpp
@@ -1,50 +1,113 @@
 #include <iostream>
 #include <string>
-#include <vector>
 
 using namespace std;
 
 class clsPerson
 {
+private:
+    string _FullName;
+
+public:
     class clsAddress
     {
+    private:
+        string _AddressLine1;
+        string _AddressLine2;
+        string _City;
+        string _Country;
+
     public:
-        string AddressLine1;
-        string AddressLine2;
-        string City;
-        string Country;
+        clsAddress(string Address1, string Address2, string City, string Country)
+        {
+            _AddressLine1 = Address1;
+            _AddressLine2 = Address2;
+            _City = City;
+            _Country = Country;
+        }
+
+        void SetAddressLine1(string Address1)
+        {
+            _AddressLine1 = Address1;
+        }
+
+        string GetAddressLine1()
+        {
+            return (_AddressLine1);
+        }
+
+        void SetAddressLine2(string Address2)
+        {
+            _AddressLine2 = Address2;
+        }
+
+        string GetAddressLine2()
+        {
+            return (_AddressLine2);
+        }
+
+        void    SetCity(string City)
+        {
+            _City = City;
+        }
+
+        string  GetCity()
+        {
+            return (_City);
+        }
+
+        void    SetCountry(string Country)
+        {
+            _Country = Country;
+        }
+
+        string  GetCountry()
+        {
+            return (_Country);
+        }
 
         void    Print()
         {
             cout << "Address:\n";
-            cout << AddressLine1 << endl;
-            cout << AddressLine2 << endl;
-            cout << City << endl;
-            cout << Country << endl; 
+            cout << "Address Line1 : " << _AddressLine1 << endl;
+            cout << "Address Line2 : " << _AddressLine2 << endl;
+            cout << "City          : " << _City << endl;
+            cout << "Country       : " << _Country << endl;
         }
     };
-public:
-    string FullName;
-    clsAddress Address;
 
-    clsPerson()
+    void  SetFullName(string FullName)
     {
-        FullName = "Koko Fofo";
-
-        Address.AddressLine1 = "Building 10";
-        Address.AddressLine2 = "Queen Street";
-        Address.City = "London";
-        Address.Country = "United Kingdom";
+        _FullName = FullName;
     }
 
-};
+    string  GetFullName()
+    {
+        return (_FullName);
+    }
 
+    // rare way of using it:
+   /*  clsAddress Address{"", "", "", ""};    
+    clsPerson(string FullName, string Address1, string Address2, string City, string Country)
+    {
+        _FullName = FullName;
+        Address = clsAddress(Address1, Address2, City, Country);
+    } */
+
+    // using initialization list //
+    clsAddress Address;
+    clsPerson(string FullName, string Address1, string Address2, string City, string Country)
+         : _FullName(FullName), Address(Address1, Address2, City, Country)
+    {
+
+    }
+};
 
 int main(void)
 {
-    clsPerson   Person1;
+    clsPerson Person1("Jacob Jonathan", "Street E", "Building03", "Ventom", "Veve");
+
     Person1.Address.Print();
 
     return (0);
 }
-
