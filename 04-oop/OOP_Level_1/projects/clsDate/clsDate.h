5228b06e58110c101da021168a59ece804d3f17f
Orwa Zain <orwazain3@gmail.com>
2026-08-20 13:50:18 +0200

    clsDate

---
+++ 04-oop/OOP_Level_1/projects/clsDate/clsDate.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include "clsString.h"
+
+class clsDate
+{
+private:
+    short   _Day = 1;
+    short   _Month = 1;
+    short   _Year = 1900;
+
+public:
+
+
+
+
+
+
+
+
+
+
+
+};
